2e2798542ec9c80b33d5fe441a73243bc4f4c9a9
Scott Wey <scottwey@users.noreply.github.com>
2024-03-20 01:22:50 -0700

    type fixes

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,9 @@ const DEFAULT_EMBEDDING_HOST: &str = "https://embedding.starpoint.ai";
 impl StarpointClient {
     pub fn new(
         api_key: &str,
-        custom_reader_host: Option<&str>,
-        custom_writer_host: Option<&str>,
-        custom_embedding_host: Option<&str>,
+        custom_reader_host: &Option<String>,
+        custom_writer_host: &Option<String>,
+        custom_embedding_host: &Option<String>,
     ) -> Self {
         let mut headers = header::HeaderMap::new();
         headers.insert(
@@ -35,13 +35,22 @@ impl StarpointClient {
             .build()
             .expect("Failed to build reqwest client for StarpointClient::new.");
 
-        let reader_host = custom_reader_host.unwrap_or(DEFAULT_READER_HOST);
+        let reader_host = custom_reader_host
+            .as_ref()
+            .map(|s| s.as_str())
+            .unwrap_or_else(|| DEFAULT_READER_HOST);
         let reader = reader::Client::new_with_client(reader_host, reqwest_client.clone());
 
-        let writer_host = custom_writer_host.unwrap_or(DEFAULT_WRITER_HOST);
+        let writer_host = custom_writer_host
+            .as_ref()
+            .map(|s| s.as_str())
+            .unwrap_or_else(|| DEFAULT_WRITER_HOST);
         let writer = writer::Client::new_with_client(writer_host, reqwest_client.clone());
 
-        let embedding_host = custom_embedding_host.unwrap_or(DEFAULT_EMBEDDING_HOST);
+        let embedding_host = custom_embedding_host
+            .as_ref()
+            .map(|s| s.as_str())
+            .unwrap_or_else(|| DEFAULT_EMBEDDING_HOST);
         let embedding = embedding::Client::new_with_client(embedding_host, reqwest_client.clone());
 
         Self {
